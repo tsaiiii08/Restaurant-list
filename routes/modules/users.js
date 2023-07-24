30df96647d71b2187372bd77f1ebf1db62956611
tsaiiii08 <lintsaiting0824@gamil.com>
2023-07-24 21:23:17 +0800

    edit "name" is not required in registration

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -21,8 +21,8 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '除名字外的欄位都是必填。' })
   }
   if (password !== confirmPassword) {
     errors.push({ message: '密碼與確認密碼不相符！' })
